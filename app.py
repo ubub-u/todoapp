762ab5a66fcd8c7d225165828559a5d07d3a6881
ubu <ubub@mailfence.com>
2021-11-10 12:27:45 -0800

    added deletes to lists

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ class TodoList(db.Model):
     __tablename__ = 'todolist'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
     todos = db.relationship('Todo', backref='list', lazy=True)
 
 
@@ -30,24 +31,85 @@ class TodoList(db.Model):
 
 # db.create_all()
 
+@app.route('/newlist/<new_list_id>deletodos')
+def list_delete_todos(new_list_id):
+    try:
+        query = TodoList.query.get(new_list_id)
+        db.session.delete(query)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print('todolist delete:', sys.exc_info())
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/newlist/<list_ids>delete', methods=['POST'])
+def list_delte(list_ids):
+    try:
+        todos = Todo.query.join(TodoList).filter(Todo.list_id==list_ids).all()
+        print(todos)
+        for each in todos:
+            db.session.delete(each)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        print('todos delete',sys.exc_info())
+    finally:
+        db.session.close()
+    return redirect(url_for('list_delete_todos', new_list_id=list_ids))
+
+
+
+
+@app.route('/newlist/<list_ids>checkboxes', methods=['POST'])
+def list_checkbox(list_ids):
+    try: 
+        completed = request.get_json()['completed']
+        query = TodoList.query.get(list_ids)
+        query.completed = completed
+        db.session.commit()
+    except:
+        db.session.rollback
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    return redirect(url_for('index'))
+
+
+@app.route('/newlist/create', methods=['POST'])
+def create_list():
+    name = request.get_json()['name']
+    list = TodoList(name=name)
+    db.session.add(list)
+    db.session.commit()
+    body = {'name': list.name}
+    db.session.close()
+    return jsonify(body['name'])
+
+
+
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
     error = False
     body = {}
     try:
         description = request.get_json()['description']
-        todo = Todo(description=description)
+        id_list = request.get_json()['list_id']
+        parent = TodoList.query.get(id_list)
+        todo = Todo(description=description, list=parent)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
+        body['list_id'] = parent.id
     except:
         db.session.rollback()
-        print(sys.exc_info())
-        error = True
+        print('is this printing?',sys.exc_info())
     finally:
         db.session.close()
-        if not error:
-            return jsonify(body)
+    return jsonify(body)
+
 
 @app.route('/todos/<todo_id>set-completed', methods=['POST'])
 def set_completed_todo(todo_id):
